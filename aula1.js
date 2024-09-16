9b4a38c3ef68f82682fb00e59cb4786f7def4d46
gabrielamargolis <gabriela.margolis@edu.pe.senac.br>
2024-09-16 22:40:17 +0000

    atualizando código

--- aula1.js
+++ aula1.js
@@ -1,1 +1,13 @@
 
+const rotulo = "natalya"
+console.log(rotulo)
+//criei meu primeiro codigo
+
+rotulo = 12345
+console.log(rotulo)
+
+const soma = 10 + 30
+const num1 = 10
+const num2 = 20
+const calc = num1 - num2
+console.log("O resultado é " + calc)
